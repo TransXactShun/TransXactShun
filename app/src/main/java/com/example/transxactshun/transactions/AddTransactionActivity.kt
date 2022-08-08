dc1e6f4bdae2aca1030d3d1001b411af78e8dbe6
Avril Gill <avrilgill@outlook.com>
2022-08-07 18:20:32 -0700

    update add transaction properties

--- app/src/main/java/com/example/transxactshun/transactions/AddTransactionActivity.kt
+++ app/src/main/java/com/example/transxactshun/transactions/AddTransactionActivity.kt
@@ -1,15 +1,20 @@
 package com.example.transxactshun.transactions
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.os.SystemClock
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.transxactshun.MainActivity
 import com.example.transxactshun.R
 import com.example.transxactshun.bills.AddReminderViewModel
 import com.example.transxactshun.database.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
@@ -19,6 +24,7 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
     private lateinit var itemNameEditText: EditText
     private lateinit var itemPriceEditText: EditText
     private lateinit var purchaseNotesEditText: EditText
+    private lateinit var vendorNameEditText: EditText
 
     // UI spinner elements
     private lateinit var spinnerPaymentType: Spinner
@@ -29,6 +35,16 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
 
     // Set current time to calendar
     private val calendar = Calendar.getInstance()
+
+
+    // Store user UID
+    private lateinit var userUID: String
+    private lateinit var userEmail: String
+    private lateinit var userPhone: String
+
+    // firebase db var
+    private lateinit var databaseFirebase: DatabaseReference
+
 
     // db
     private lateinit var database: ExpensesDatabase
@@ -43,6 +59,15 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
         saveNewTransactionBtn = findViewById(R.id.save_new_transaction)
         cancelNewTransactionBtn = findViewById(R.id.cancel_new_transaction)
 
+        // retrieve current user email
+        val user = Firebase.auth.currentUser
+        if (user != null) {
+            userUID = user.uid
+            userEmail = user.email.toString()
+        } else {
+            returnToLoginScreen()
+        }
+
         //init view model
         addTransactionViewModel = ViewModelProvider(this).get(AddTransactionViewModel::class.java)
 
@@ -54,9 +79,11 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
         // Setup EditText fields
         itemNameEditText = findViewById(R.id.addItemName)
         itemPriceEditText = findViewById(R.id.addItemPrice)
+        vendorNameEditText = findViewById(R.id.addVendorName)
         purchaseNotesEditText = findViewById(R.id.addPurchaseNotes)
         spinnerPaymentType = findViewById(R.id.spinnerPaymentMethod)
         spinnerCategoryType = findViewById(R.id.spinnerItemCategoryMethod)
+
 
         // add data to spinner
         spinnerPaymentType.adapter = ArrayAdapter<PaymentType>(
@@ -83,6 +110,8 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
 
             try {
                 // Prepare data
+                var itemName = itemNameEditText.text.toString()
+                var vendorName = vendorNameEditText.text.toString()
                 var price = 0
                 val priceString = itemPriceEditText.text.toString()
                 if (!priceString.isNullOrBlank()) {
@@ -97,14 +126,14 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
 
                 // Creating the expense data object
                 val expensesObj = ExpensesDatabaseEntry(
-                    email = "test@abc.com",
+                    email = userEmail,
                     category = itemCategories,
                     note = notes,
                     paymentType = paymentMethod,
                     epochDate = theDate,
                     cost = price,
-                    vendor = "testing",
-                    items = "test"
+                    vendor = vendorName,
+                    items = itemName
                 )
 
                 println("debug: $expensesObj")
@@ -118,6 +147,9 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
                     Toast.LENGTH_SHORT
                 )
                     .show()
+
+                calculateBudget()
+
             } catch (ex: Exception) {
                 Toast.makeText(
                     this,
@@ -139,6 +171,32 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
         super.onSaveInstanceState(savedInstanceState)
     }
 
+    private fun calculateBudget() {
+        try {
+
+
+        } catch (ex: Exception) {
+            Toast.makeText(
+                baseContext,
+                "Unable to calculate budget!",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
+    }
+
+    private fun sendSmsNotification() {
+        try {
+
+        } catch (ex: Exception) {
+            Toast.makeText(
+                baseContext,
+                "Unable to generate text notification, maybe your API account has lower balance!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
 
     fun onClickDateTransaction(view: View) {
         val datePickerDialog = DatePickerDialog(
@@ -153,5 +211,20 @@ class AddTransactionActivity : AppCompatActivity(), DatePickerDialog.OnDateSetLi
         addTransactionViewModel.dateTransaction?.set(Calendar.MONTH, monthOfYear)
         addTransactionViewModel.dateTransaction?.set(Calendar.DAY_OF_MONTH, dayOfMonth)
     }
+
+    private fun returnToLoginScreen() {
+        Toast.makeText(
+            baseContext,
+            "User logged out!",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        // return to main activity
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        this.finish()
+
+    }
+
 
 }
